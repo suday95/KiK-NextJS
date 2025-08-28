b18bdb2d4cfbf8ed2bae7ed9367597b2f3b8eb51
Aashrith-Vellampalli <aashrith.vellampalli.24@kgpian.iitkgp.ac.in>
2025-08-28 19:34:00 +0530

    Revert "New landing page"

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,9 @@ const nextConfig = {
     remotePatterns: [
       new URL("https://github.com/kodeinkgp/Photos/blob/main/**?raw=true"),
       new URL("https://**.githubusercontent.com/**"),
-      new URL(
-        "https://github.com/KodeinKGP-Technology-Web-3-0-Society/Photos/blob/main/2025_SETS/**?raw=true"
-      ),
-    ],
-  },
+      new URL("https://github.com/KodeinKGP-Technology-Web-3-0-Society/Photos/blob/main/2025_SETS/**?raw=true")
+    ]
+  }
 };
 
 export default nextConfig;
